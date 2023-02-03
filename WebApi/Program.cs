8b8370b6739d816528602a04911cc33585953c44
a-pedroso <andre.pedroso@outlook.com>
2023-02-03 01:41:23 +0000

    add cat fact http client

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,8 +1,11 @@
 using Grpc.Net.Client;
 using GrpcService;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Resources;
+using System.Net.Http;
+using System.Text.Json;
 using WebApi.Extensions;
 
 using var loggerFactory = LoggerFactory.Create(builder =>
@@ -24,6 +27,11 @@ try
     builder.SetupLogging();
 
     // Add services to the container.
+    builder.Services.AddHttpClient("CatFact", httpClient =>
+    {
+        httpClient.BaseAddress = new Uri("https://catfact.ninja/");
+    });
+
     builder.Services.AddOpenTelemetryExtension(builder.Configuration, builder.Environment);
 
     builder.Services.AddEndpointsApiExplorer();
@@ -55,24 +63,45 @@ try
                 summaries[Random.Shared.Next(summaries.Length)]
             ))
             .ToArray();
-        return forecast;
+        return Results.Ok(forecast);
     })
     .WithName("GetWeatherForecast")
     .WithOpenApi();
+
+    app.MapGet("/catfact", async (ILoggerFactory loggerFactory, IHttpClientFactory httpClientFactory) =>
+    {
+        ILogger loggerCatFact = loggerFactory.CreateLogger("CatFact");
+        loggerCatFact.LogInformation("Get Cat Fact");
+
+        var httpClient = httpClientFactory.CreateClient("CatFact");
+        var httpResponseMessage = await httpClient.GetAsync("fact");
+
+        if (httpResponseMessage.IsSuccessStatusCode)
+        {
+            var content = await httpResponseMessage.Content.ReadFromJsonAsync<CatFact>();
+            return Results.Ok(content);
+        }
+        else
+        {
+            loggerCatFact.LogWarning("failed to retrieve cat fact");
+            return Results.Problem(statusCode:503);
+        }
+    })
+    .WithName("GetCatFact")
+    .WithOpenApi();
+
 
     app.MapGet("/sayhello", async ([FromQuery] string name, IConfiguration config, ILoggerFactory loggerFactory) =>
     {
         ILogger loggerSayHello = loggerFactory.CreateLogger("SayHello");
         loggerSayHello.LogInformation("Get sayhello to {name}", name);
 
-        loggerSayHello.LogError("Testing error log entry");
-
         string? uri = config.GetValue<string>("GrpcService:Uri");
         ArgumentNullException.ThrowIfNull(uri);
         using var channel = GrpcChannel.ForAddress(uri);
         var client = new Greeter.GreeterClient(channel);
         var reply = await client.SayHelloAsync(new HelloRequest { Name = name });
-        return reply.Message;
+        return Results.Ok(reply.Message);
     })
     .WithName("GetSayHello")
     .WithOpenApi();
@@ -92,3 +121,5 @@ internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+
+internal record CatFact(string Fact, int Length);
